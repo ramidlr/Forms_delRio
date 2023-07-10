15a105e35f925e313f3426952fac7c38674b270b
ramidlr <ramidlr@ramidlrs-MacBook-Pro.local>
2023-07-10 14:27:28 +0200

    oneoneone

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# FormsDelRio
+# Myapp
 
 This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 16.1.0.
 
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'forms_delRio'`, () => {
+  it(`should have as title 'myapp'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('forms_delRio');
+    expect(app.title).toEqual('myapp');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('forms_delRio app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('myapp app is running!');
   });
 });
