b1b3f93f344bde81bfd8d718984c3c1691fad628
ramidlr <ramidlr@ramidlrs-MacBook-Pro.local>
2023-07-10 14:49:18 +0200

    form module y reactiveformcomponent creados

---
+++ src/app/form/form.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ReactiveformComponent } from './reactiveform/reactiveform.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+
+
+@NgModule({
+  declarations: [
+    ReactiveformComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    ReactiveformComponent
+  ]
+})
+export class FormModule { }
